143ef7caaf9e68103b5c2ef5f49348fe48810718
Ritesh20patil <140839884+Ritesh20patil@users.noreply.github.com>
2025-06-25 20:41:17 +0530

    Data transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.Data_Science import logger
 from src.Data_Science.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.Data_Science.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.Data_Science.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -22,3 +25,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
