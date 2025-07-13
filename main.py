d9c5960bbf5fbaa121e93e84a28d08e85ba749ea
Ritesh20patil <140839884+Ritesh20patil@users.noreply.github.com>
2025-07-13 18:30:49 +0530

    model trainer and pipeline added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.Data_Science import logger
 from src.Data_Science.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.Data_Science.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.Data_Science.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from src.Data_Science.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -37,3 +37,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.initiate_model_training()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
